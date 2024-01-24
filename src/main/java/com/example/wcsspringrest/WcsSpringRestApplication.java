785d2a4f211b76bb4be95452d6282487055b0361
arseniit <arseniit@gmail.com>
2024-01-24 14:00:04 +0100

    Initial commit

---
+++ src/main/java/com/example/wcsspringrest/WcsSpringRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.wcsspringrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WcsSpringRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WcsSpringRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wcsspringrest/WcsSpringRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wcsspringrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WcsSpringRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
